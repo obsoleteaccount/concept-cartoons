0fba20a43eeca53d105efb9d129d5ed28834f2e9
Shining <shiningwanj@gmail.com>
2019-10-04 13:04:38 +0300

    Updates

---
+++ precache-manifest.7fb9aa71269b0564615ce3c7807448a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5bd10f553f3aa0963b63666a28138a0",
+    "url": "/concept-cartoons/index.html"
+  },
+  {
+    "revision": "fe187516daccf50f8526",
+    "url": "/concept-cartoons/static/css/main.20cdee51.chunk.css"
+  },
+  {
+    "revision": "02cf02eed451b32027d0",
+    "url": "/concept-cartoons/static/js/2.c762ee2a.chunk.js"
+  },
+  {
+    "revision": "fe187516daccf50f8526",
+    "url": "/concept-cartoons/static/js/main.608e98a3.chunk.js"
+  },
+  {
+    "revision": "e67119190056eb922da8",
+    "url": "/concept-cartoons/static/js/runtime-main.3c602a46.js"
+  }
+]);
